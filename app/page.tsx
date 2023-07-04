202c532cfed0923096e6769fd2ac9fbb586c7e24
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-07-04 22:36:36 +0800

    added music.mp3

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <main className="relative px-10 md:px-20 py-14 max-width">
       <div className="flex items-center justify-center w-full ">
-        <video src="/vid.mp4" loop={true} autoPlay={true} />
+        <video loop autoPlay playsInline muted>
+          <source src="vid.mp4" type="video/mp4" />
+        </video>
+        <audio autoPlay loop hidden playsInline controls>
+          <source src="music.mp3" type="audio/mpeg" />
+        </audio>
       </div>
       <Form />
     </main>
