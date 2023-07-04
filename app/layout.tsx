a443ed0f4b3003c6900d63af5afa394c7721b798
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-07-04 22:02:04 +0800

    added react-player

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className="transition-all duration-500">
         <Providers>
           <Navbar />
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { Form } from "@/components";
-import Image from "next/image";
+import ReactPlayer from "react-player";
 
 export default function Home() {
   return (
     <main className="relative px-10 md:px-20 py-14 max-width">
-      <div className="w-full flex justify-center items-center ">
-        <video src="/vid.mp4" autoPlay loop></video>
+      <div className="flex items-center justify-center w-full ">
+        <ReactPlayer
+          url="vid.mp4"
+          playing={true}
+          loop={true}
+          controls={false}
+        />
       </div>
       <Form />
     </main>
