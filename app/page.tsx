99881e12d82779463fb17b612a1db964e0bcfa51
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-07-04 22:18:36 +0800

    removed react-player

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { Form } from "@/components";
-import ReactPlayer from "react-player";
 
 export default function Home() {
   return (
     <main className="relative px-10 md:px-20 py-14 max-width">
       <div className="flex items-center justify-center w-full ">
-        <video src="vid.mp4" loop={true} autoPlay={true} />
+        <video src="/vid.mp4" loop={true} autoPlay={true} />
       </div>
       <Form />
     </main>
